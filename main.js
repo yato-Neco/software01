0e9dacfb504bec12218d452fe11bcc50107561a8
x64neco <yatoneco0015@gmail.com>
2024-12-04 13:15:28 +0900

    add method

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 class Money {
     constructor() {
         this.money = 0;
+        this.input_money = 0;
+    }
+
+    check() {
+        return this.money
     }
 }
 
 class Gacha {
     constructor() {
-        
+        this.money_class = Money();
+        this.rotate_sound = null;
+        this.exhaust_sound = null;
+        this.rarity_list = null;
+        this.design = null;
+        this.items = [Item(),Item(),Item(),Item()];
     }
 }
 
@@ -16,14 +26,44 @@ class Item {
         this.name = null;
         this.rarity = Rarity();
     }
+
+    get() {
+        return this
+    }
+
 }
 
 class MiniGame {
+    constructor() {
+        this.quiz = ["",""];
+        this.awnsor = "";
+    }
 
+    start(params) {
+        
+    }
+
+    check(index) {
+        return this.quiz[index] == this.awnsor;
+
+    }
 }
 
 class Rarity {
     constructor() {
-
+        this.rarity = 0;
+        this.probability = 0.0;
+        this.up_probability = 0.0;
     }
+
+    get() {
+        return this
+    }
+
+    set(_this) {
+        this = _this
+        return true
+    }
+    
 }
+
