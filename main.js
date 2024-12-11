90b9b5afd15a50443ce968e2f07ea480844d9136
x64neco <yatoneco0015@gmail.com>
2024-12-11 11:51:03 +0900

    add sound

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-console.log("hello")
+console.log("hello");
 let minigame = new MiniGame();
 let gacha = new Gacha();
+localStorage.setItem("name", "あ");
 
 function MiniGameStart() {
     console.log("MiniGameStart");
@@ -8,13 +9,38 @@ function MiniGameStart() {
     let isRight = minigame.check();
     console.log(`isRight: ${isRight}`);
     if (isRight) {
-        
     }
-    
+
+    gacha.start();
     gacha.rotate();
-
     gacha.discharge();
-
-
 }
 
+function Test0() {
+    console.log(localStorage.getItem("name"));
+}
+
+function Test1() {
+    localStorage.setItem("name", "a");
+    console.log(localStorage.getItem("name"));
+}
+function lot() {
+    const data = {
+        "SSR": 30,
+        "SR": 40,
+    };
+    const rand = Math.floor(Math.random() * 100);
+    let result = "ノーマル";
+    let rate = 0;
+    for (const prop in data) {
+        rate += data[prop];
+        if (rand <= rate) {
+            result = prop;
+            break;
+        }
+    }
+    return result;
+   
+}
+
+console.log(lot());
--- someclass.js
+++ someclass.js
@@ -1,7 +1,7 @@
 class Money {
     constructor() {
         this.money = 0;
-        this.input_money = 0;
+        this.input_money = 500;
     }
 
     check() {
@@ -14,23 +14,34 @@ class Gacha {
         this.money_class = new Money();
         this.rotate_sound = rotate_sound;
         this.discharge_sound = discharge_sound;
-        this.rarity_list = null;
+
+        let r0 = new Rarity();
+        let r1 = new Rarity();
+        let r2 = new Rarity();
+        let r3 = new Rarity();
+
+        
         this.design = design;
-        this.items = [new Item(),new Item(),new Item(),new Item()];
+        this.items = [new Item(r0),new Item(r1),new Item(r2),new Item(r3)];
     }
 
-    
+    start() {
+        this.money_class.money += this.money_class.input_money;
+        localStorage.setItem("money", `${this.money_class.money}`);
+        console.log(`ガチャ内金額 ${this.money_class.money}`)
+    }
     
     change_design() {
         
     }
 
     play_rotate_sound() {
-
+        
     }
 
     play_discharge_sound() {
-
+        let sound = document.getElementById("discharge");
+        sound.play();
     }
 
     rotate() {
@@ -42,40 +53,79 @@ class Gacha {
     }
 
     discharge() {
-        gacha.play_discharge_sound();
 
         let item = document.getElementById("item");
         item.src = "./image/capsule_close1_red.png";
         let task;
         let i = 1;
 
+        
+        const data = {
+            "SSR": 30,
+            "SR": 40,
+        };
+        const rand = Math.floor(Math.random() * 100);
+        let result = "ノーマル";
+        let rate = 0;
+        for (const prop in data) {
+            rate += data[prop];
+            if (rand <= rate) {
+                result = prop;
+                break;
+            }
+        }
+
+        console.log(result);
+
+
+        
         task = setInterval(() => {
             console.log(`item zoom: ${i}`);
 
             item.width = "" + 100 * i;
-            if (i > 3) {
 
+            if (i > 3) {
                 
+                document.getElementById("");
+                
+                gacha.play_discharge_sound();
+
+
                 clearInterval(task);
             }
 
             i++;
         },1000);
+    }
 
 
-
+    up_rarity() {
+        
     }
 }
 
 class Item {
     image = "";
-    name = "";
+    _name = "";
     rarity = null;
     
-    constructor() {
+    constructor(rarity) {
         this.image = null;
-        this.name = null;
-        this.rarity = new Rarity();
+        this._name = null;
+        this.rarity = rarity;
+        
+    }
+
+    set_image(image) {
+        this.image = image;
+    }
+
+    set_rarity(index) {
+        this.rarity = rarity_list[index];
+    }
+
+    set_name(_name) {
+        this._name = _name;
     }
 
     get() {
@@ -109,6 +159,10 @@ class Rarity {
         this.rarity = 0;
         this.probability = 0.0;
         this.up_probability = 0.0;
+    }
+
+    up_rarity() {
+
     }
 
     get() {
