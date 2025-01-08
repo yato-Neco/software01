fbbe24e6b1add4f0b920f7a6288d2d7a609b4d45
x64neco <yatoneco0015@gmail.com>
2025-01-08 11:53:40 +0900

    アイテム10個拡張

--- someclass.js
+++ someclass.js
@@ -4,70 +4,79 @@ class Money {
         if (localStorage.getItem("money") == null) {
             localStorage.setItem("money", `${0}`);
         }
-        
     }
 
     check(id) {
-        let money = localStorage.getItem("money")
-        
+        let money = localStorage.getItem("money");
+
         if (id != null || id != undefined) {
-            document.getElementById(id).innerText = money
+            document.getElementById(id).innerText = money;
         }
-        
-        return money
+
+        return money;
     }
 
     pull(id) {
         console.log("お金を引き出しました");
         localStorage.setItem("money", `${0}`);
         if (id != null || id != undefined) {
-            document.getElementById(id).innerText = localStorage.getItem("money")
+            document.getElementById(id).innerText = localStorage.getItem(
+                "money",
+            );
         }
     }
 
     push(money) {
         console.log("お金を受け取りました");
         localStorage.setItem("money", `${money + this.input_money}`);
-
     }
-
 }
 
 class Gacha {
-   
     designs = [
         "position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1;",
         "position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1; filter: hue-rotate(90deg);",
-        "position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1; filter: hue-rotate(180deg);"
-    ]
-    constructor(rotate_sound,discharge_sound) {
+        "position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1; filter: hue-rotate(180deg);",
+    ];
+    constructor(rotate_sound, discharge_sound) {
         this.money_class = new Money();
         this.rotate_sound = rotate_sound;
         this.discharge_sound = discharge_sound;
 
-        let r0 = new Rarity(0,0.1,"R");
-        let r1 = new Rarity(1,0.1,"R");
-        let r2 = new Rarity(2,0.2,"R");
-        let r3 = new Rarity(3,0.2,"N");
-        let r5 = new Rarity(3,0.2,"N");
-        let r6 = new Rarity(3,0.2,"N");
-        let r7 = new Rarity(3,0.2,"N");
-        let r8 = new Rarity(3,0.2,"N");
-        let r9 = new Rarity(3,0.2,"N");
+        let r0 = new Rarity(0, 0.1, "R");
+        let r1 = new Rarity(1, 0.1, "R");
+        let r2 = new Rarity(2, 0.2, "R");
+        let r3 = new Rarity(3, 0.2, "N");
+        let r4 = new Rarity(3, 0.2, "N");
+        let r5 = new Rarity(3, 0.2, "N");
+        let r6 = new Rarity(3, 0.2, "N");
+        let r7 = new Rarity(3, 0.2, "N");
+        let r8 = new Rarity(3, 0.2, "N");
+        let r9 = new Rarity(3, 0.2, "N");
 
+        this.items = [
+            new Item(r0, 0),
+            new Item(r1, 1),
+            new Item(r2, 2),
+            new Item(r3, 3),
+            new Item(r4, 4),
+            new Item(r5, 5),
+            new Item(r6, 6),
+            new Item(r7, 7),
+            new Item(r8, 8),
+            new Item(r9, 9),
+        ];
 
-    
-        this.items = [new Item(r0,0),new Item(r1,1),new Item(r2,2),new Item(r3,3)];
-
-       
-        
-        const designIndex = localStorage.getItem("design")
+        const designIndex = localStorage.getItem("design");
         if (designIndex == null) {
             localStorage.setItem("design", `${0}`);
         }
-        const gacha  = document.getElementById("gachagacha");
+        const gacha = document.getElementById("gachagacha");
         if (gacha != null) {
-            gacha.setAttribute("style", this.designs[localStorage.getItem("design")]);
+            gacha.setAttribute(
+                "style",
+                this.designs[localStorage.getItem("design")],
+            );
         }
     }
 
@@ -75,38 +84,36 @@ class Gacha {
         let money = parseInt(this.money_class.check());
         this.money_class.push(money);
 
-
         minigame.start();
         let isRight = minigame.check();
         console.log(`isRight: ${isRight}`);
         if (isRight) {
-            this.up_rarity(12.0)
+            this.up_rarity(12.0);
         }
-    
-             
+
         this.rotate();
 
         let item = document.getElementById("item");
         item.src = "./image/capsule_close1_red.png";
         let task;
         let i = 1;
-        const dialog = document.querySelector('#myDialog');
-        const okButton = document.querySelector('#myDialog .button.ok');
+        const dialog = document.querySelector("#myDialog");
+        const okButton = document.querySelector("#myDialog .button.ok");
         const ItemName = document.getElementById("showItemName");
         const ItemImage = document.getElementById("showItemImage");
         const Rarity = document.getElementById("rarity");
 
-        okButton.addEventListener('click', function () {
+        okButton.addEventListener("click", function () {
             dialog.close();
-            item.width = "0"
+            item.width = "0";
         });
-        
-        
+
         const rand = Math.floor(Math.random() * 100);
         let result = "3";
         let rate = 0;
         for (const prop in this.items) {
-            rate += this.items[prop].rarity.get_probability(parseInt(prop)) * 100;
+            rate += this.items[prop].rarity.get_probability(parseInt(prop)) *
+                100;
             if (rand <= rate) {
                 result = prop;
                 break;
@@ -114,45 +121,42 @@ class Gacha {
         }
 
         console.log(`result: ${result}`);
-        
-        
+
         task = setInterval(() => {
             //console.log(`item zoom: ${i}`);
 
             item.width = "" + 100 * i;
 
             if (i > 3) {
-                item.width = "0"
+                item.width = "0";
                 gacha.play_discharge_sound();
 
-                
                 dialog.show();
 
-                const ditem = this.items[parseInt(result)]
-                ItemName.innerText = ditem.get_name()
-                ItemImage.src = ditem.get_image()
-                Rarity.innerText = "レアリティ: " +  this.items[result].rarity.get_rarity();
+                const ditem = this.items[parseInt(result)];
+                ItemName.innerText = ditem.get_name();
+                ItemImage.src = ditem.get_image();
+                Rarity.innerText = "レアリティ: " +
+                    this.items[result].rarity.get_rarity();
 
                 for (const prop in this.items) {
-                    this.items[prop].rarity.rest_probability(parseInt(prop),1.0)
+                    this.items[prop].rarity.rest_probability(
+                        parseInt(prop),
+                        1.0,
+                    );
                 }
                 clearInterval(task);
             }
 
             i++;
-        },1000);
-
-
+        }, 1000);
     }
-    
+
     change_design() {
-       
-        const select = document.getElementById('change_design');
-        console.log(select.value)
-        localStorage.setItem(`design`, `${select.value}`)
-
+        const select = document.getElementById("change_design");
+        console.log(select.value);
+        localStorage.setItem(`design`, `${select.value}`);
     }
-
 
     play_discharge_sound() {
         let sound = document.getElementById("discharge");
@@ -160,29 +164,22 @@ class Gacha {
     }
 
     rotate() {
-
-
-
         gacha.play_rotate_sound();
 
-        const gachaImage = document.getElementById('gachagacha');
+        const gachaImage = document.getElementById("gachagacha");
 
-        gachaImage.classList.toggle('rotated');
+        gachaImage.classList.toggle("rotated");
     }
 
-
-    
     play_rotate_sound() {
-        
     }
-
 
     up_rarity(up_probability) {
-
         for (let i = 0; i < this.items.length; i++) {
             if (this.items[i].rarity != "N") {
-                const up = this.items[i].rarity.get_probability(i) * up_probability
-                this.items[i].rarity.set_probability(i,up)
+                const up = this.items[i].rarity.get_probability(i) *
+                    up_probability;
+                this.items[i].rarity.set_probability(i, up);
             }
         }
     }
@@ -192,41 +189,46 @@ class Gacha {
         const selectedValue = selectElement.value;
         const inputElement = document.getElementById("Inputprobability");
         const inputValue = inputElement.value.trim();
-        console.log(inputValue)
+        console.log(inputValue);
 
-        if (inputValue.trim() !== "" && inputValue !== null && inputValue !== undefined && !isNaN(inputValue)) {
+        if (
+            inputValue.trim() !== "" && inputValue !== null &&
+            inputValue !== undefined && !isNaN(inputValue)
+        ) {
+            this.items[selectedValue].rarity.set_read_probability(
+                parseInt(selectedValue),
+                parseFloat(inputValue),
+            );
 
-            this.items[selectedValue].rarity.set_read_probability(parseInt(selectedValue),parseFloat(inputValue))
-        
-            console.log(this.items[selectedValue].rarity.get_probability(0))
-        }    
+            console.log(this.items[selectedValue].rarity.get_probability(0));
+        }
     }
 }
 
 class Item {
     rarity = null;
-    index = 0
-    constructor(rarity,index) {
-        this.index = index
+    index = 0;
+    constructor(rarity, index) {
+        this.index = index;
         this.rarity = rarity;
-        
 
-        localStorage.setItem(`item${index}_image`, "./image/onepiece01_luffy2.png");
-        localStorage.setItem(`item${index}_name`, "item"+index);
+        localStorage.setItem(
+            `item${index}_image`,
+            "./image/onepiece01_luffy2.png",
+        );
+        localStorage.setItem(`item${index}_name`, "item" + index);
     }
 
-    
-
     get_name() {
-        return localStorage.getItem(`item${this.index}_name`)
+        return localStorage.getItem(`item${this.index}_name`);
     }
 
     get_image() {
-        return localStorage.getItem(`item${this.index}_image`)
+        return localStorage.getItem(`item${this.index}_image`);
     }
-    
+
     get_rarity() {
-        return this.rarity.get_rarity()
+        return this.rarity.get_rarity();
     }
 }
 
@@ -237,13 +239,15 @@ class MiniGame {
 
     constructor() {
         this.quiz = "0 or 1";
-        this.choices = ["0","1"]
+        this.choices = ["0", "1"];
         this.response = "";
         this.awnsor = "0";
     }
 
     start() {
-        this.response = window.prompt(`${this.choices[0]} \n ${this.choices[0]} or ${this.choices[1]}`);
+        this.response = window.prompt(
+            `${this.choices[0]} \n ${this.choices[0]} or ${this.choices[1]}`,
+        );
     }
 
     check() {
@@ -252,53 +256,76 @@ class MiniGame {
 }
 
 class Rarity {
-    index = 0
-    constructor(index,probability,rarity) {
+    index = 0;
+    constructor(index, probability, rarity) {
         this.rarity = rarity;
         this.probability = probability;
         this.up_probability = probability;
         this.index = index;
-        
-        
 
-        if (localStorage.getItem(`rarity${index}_probability`) == null || localStorage.getItem(`rarity${index}_probability`) == NaN) {
-            localStorage.setItem(`rarity${index}_probability`, `${probability}`);
+        if (
+            localStorage.getItem(`rarity${index}_probability`) == null ||
+            localStorage.getItem(`rarity${index}_probability`) == NaN
+        ) {
+            localStorage.setItem(
+                `rarity${index}_probability`,
+                `${probability}`,
+            );
         }
 
-        if (localStorage.getItem(`rarity${index}_readonly_probability`) == null || localStorage.getItem(`rarity${index}_readonly_probability`) == NaN) {
-            localStorage.setItem(`rarity${index}_readonly_probability`, `${probability}`);
+        if (
+            localStorage.getItem(`rarity${index}_readonly_probability`) ==
+                null ||
+            localStorage.getItem(`rarity${index}_readonly_probability`) == NaN
+        ) {
+            localStorage.setItem(
+                `rarity${index}_readonly_probability`,
+                `${probability}`,
+            );
         }
         if (false) {
-            console.log("init")
-            localStorage.setItem(`rarity${index}_readonly_probability`, `${probability}`);
-            localStorage.setItem(`rarity${index}_probability`, `${probability}`);
+            console.log("init");
+            localStorage.setItem(
+                `rarity${index}_readonly_probability`,
+                `${probability}`,
+            );
+            localStorage.setItem(
+                `rarity${index}_probability`,
+                `${probability}`,
+            );
         }
     }
 
-    rest_probability(index,probability) {
-        localStorage.setItem(`rarity${index}_probability`, `${parseFloat(localStorage.getItem(`rarity${index}_readonly_probability`))}`);
-       
+    rest_probability(index, probability) {
+        localStorage.setItem(
+            `rarity${index}_probability`,
+            `${
+                parseFloat(
+                    localStorage.getItem(`rarity${index}_readonly_probability`),
+                )
+            }`,
+        );
     }
 
-    set_read_probability(index,probability) {
-        localStorage.setItem(`rarity${index}_readonly_probability`, `${probability}`);
+    set_read_probability(index, probability) {
+        localStorage.setItem(
+            `rarity${index}_readonly_probability`,
+            `${probability}`,
+        );
         localStorage.setItem(`rarity${index}_probability`, `${probability}`);
     }
 
-
     get_probability(index) {
         //console.log(parseFloat(localStorage.getItem(`rarity${index}_probability`)))
-        return parseFloat(localStorage.getItem(`rarity${index}_probability`))
+        return parseFloat(localStorage.getItem(`rarity${index}_probability`));
     }
 
-    set_probability(index,probability) {
-        localStorage.setItem(`rarity${index}_probability`, `${probability}`);        
+    set_probability(index, probability) {
+        localStorage.setItem(`rarity${index}_probability`, `${probability}`);
         //console.log(parseFloat(localStorage.getItem(`rarity${index}_probability`)))
     }
 
     get_rarity() {
-        return this.rarity
+        return this.rarity;
     }
-   
 }
-
